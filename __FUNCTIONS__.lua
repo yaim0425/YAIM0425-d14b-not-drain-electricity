9604ead420b2083a77f84a79e99db46a8fe3291a
yaim0425 <yaim0425@gmail.com>
2025-09-06 18:14:35 -0500

    Se agrega la validación de _G.settings

--- __FUNCTIONS__.lua
+++ __FUNCTIONS__.lua
@@ -15,6 +15,7 @@ if GMOD.copy then return end
 
 --- Para que Visual Studio Code no moleste
 _G.log = _G.log or function(...) end
+_G.settings = _G.settings or {}
 
 ---------------------------------------------------------------------------------------------------
 
