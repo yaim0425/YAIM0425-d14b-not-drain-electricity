124b12ef98136592c24e369900043939edbb2b58
yaim0425 <yaim0425@gmail.com>
2025-09-15 18:06:12 -0500

    Corrección en la creación de los prototipos de las recetas

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -360,7 +360,6 @@ function GMOD.extend(...)
 
             GMOD.recipes[prototype.name] = GMOD.recipes[prototype.name] or {}
             table.insert(GMOD.recipes[prototype.name], prototype)
-            prototype.enabled = prototype.enabled or true
             return
         end
 
